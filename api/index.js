d5e0c1ca918c5e7b7b48274ef0751e58491d11b5
hamzahh09 <84889114+hamzahh09@users.noreply.github.com>
2021-05-27 11:01:42 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const model = require('./sdk/model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1733547356:AAEOX7oG_z09vS34M-DUHOm5YCPsXYDXohg'
+const token = '1733199726:AAH8dPwgZj-U5ClT8tU9XkISoQ1l-00g1Zg'
 const bot = new TelegramBot(token, {polling: true});
 
 
