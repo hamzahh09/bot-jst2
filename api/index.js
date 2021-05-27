ce6bc4322d3038618142e7386bd8cc5c6c17f705
hamzahh09 <84889114+hamzahh09@users.noreply.github.com>
2021-05-27 13:07:41 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-ar express = require('express');
+var express = require('express');
 var r = express.Router();
 
 // load pre-trained model
@@ -16,41 +16,17 @@ bot.onText(/\/start/, (msg) => {
     bot.sendMessage(
         msg.chat.id,
         `hello ${msg.chat.first_name}, welcome...\n
-        click /predict`
+        click /menu to main menu`
     );   
 });
-state = 0;
-bot.onText(/\/predict/, (msg) => { 
+
+bot.onText(/\/menu/, (msg) => { 
+    console.log(msg)
     bot.sendMessage(
         msg.chat.id,
-        `masukkan nilai i|v contohnya 9|9`
+        `this is your main menu`
     );   
-    state = 1;
 });
-bot.on('message', (msg) => {
-    if(state ==1){
-        s = msg.text.split("|");
-        i = s[0]
-        v = s[1]
-        model.predict(
-            [
-                parseFloat(s[0]), //string to float
-                parseFloat(s[1])
-            ]
-        ).then(jres)=>{
-            bot.sendMessage(
-                msg.chat.id,
-                `nilai v yang diprediksi adalah ${jres[0]} volt`
-            );
-            bot.sendMessage(
-                msg.chat.id,
-                `nilai p yang diprediksi adalah ${jres[1]} watt`
-            );
-        })
-    }else{
-        state = 0
-    }
-})
 
 // routers
 r.get('/prediction/:i/:r', function(req, res, next) {    
